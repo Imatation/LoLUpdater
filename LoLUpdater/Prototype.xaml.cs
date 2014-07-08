f9aa432de02e25e99666762542a89916f823888b
JD Davis <makdaddydavisjr@sbcglobal.net>
2014-07-08 11:18:09 -0500

    Attaching missing files

---
+++ LoLUpdater/Prototype.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoLUpdater
+{
+	/// <summary>
+	/// Interaction logic for Prototype.xaml
+	/// </summary>
+	public partial class Prototype : Window
+	{
+		public Prototype()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
