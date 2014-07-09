6e0ef8b004ffdc4cfb4a8f96dbf817af53b720c4
David Knaack <david@knaack.me>
2014-07-09 18:15:27 +0200

    new website
    blog and automated building with grunt or gulp still missing

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+if (document.getElementsByTagName("body")[0].className.indexOf("xp") > -1) {
+  var download = document.getElementById("download"),
+      downloadAlt = document.getElementById("download-alt"),
+
+  download.href = "download-xp.html";
+  downloadAlt.href = "download-vista.html";
+}
