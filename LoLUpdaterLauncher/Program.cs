8daf201cb42ee208cbcd018fe7308face7272689
Ilja Korsun <ilja.korsun@gmail.com>
2014-07-18 05:15:42 +0200

    Shyt

--- LoLUpdaterLauncher/Program.cs
+++ LoLUpdaterLauncher/Program.cs
@@ -7,13 +7,13 @@ namespace LoLUpdater
     {       
         static void Main(string[] args)
         {
-            File.WriteAllBytes("tbb.dll", LoLUpdater.Properties.Resources.tbb);
-            File.WriteAllBytes("Cg_3_1_April2012_Setup.exe", LoLUpdater.Properties.Resources.Cg_3_1_April2012_Setup);
-            File.WriteAllText("NvidiaCGLicence.txt", LoLUpdater.Properties.Resources.NvidiaCGLicence);
+            File.WriteAllBytes("tbb.dll", Properties.Resources.tbb);
+            File.WriteAllBytes("Cg_3_1_April2012_Setup.exe", Properties.Resources.Cg_3_1_April2012_Setup);
+            File.WriteAllText("NvidiaCGLicence.txt", Properties.Resources.NvidiaCGLicence);
             Version winvistaversion = new Version(6, 0, 6000, 0);
             if (System.Environment.OSVersion.Platform == PlatformID.Win32NT && Environment.OSVersion.Version >= winvistaversion)
             {
-                File.WriteAllBytes("LoLUpdater.exe", LoLUpdater.Properties.Resources.LoLUpdater);
+                File.WriteAllBytes("LoLUpdater.exe", Properties.Resources.LoLUpdater);
                 var cmd = new ProcessStartInfo();
                 var process = new Process();
                 cmd.FileName = "LoLUpdater.exe";
@@ -23,7 +23,7 @@ namespace LoLUpdater
             }
             else if (System.Environment.OSVersion.Platform == PlatformID.Win32NT && Environment.OSVersion.Version < winvistaversion || Environment.OSVersion.Version == null)
             {
-                File.WriteAllBytes("LoLUpdaterXP.exe", LoLUpdater.Properties.Resources.LoLUpdaterXP);
+                File.WriteAllBytes("LoLUpdaterXP.exe", Properties.Resources.LoLUpdaterXP);
                 Process.Start("LoLUpdaterXP.exe");
             }
         }
