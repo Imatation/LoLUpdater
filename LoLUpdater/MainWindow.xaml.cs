d2f9448ca04727cff3d9fdcda81d67f84fd48116
Ilja Korsun <ilja.korsun@gmail.com>
2014-07-23 02:20:53 +0200

    resource files

--- LoLUpdater/MainWindow.xaml.cs
+++ LoLUpdater/MainWindow.xaml.cs
@@ -6,10 +6,22 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using WUApiLib;
+using System.Globalization;
+using System.Resources;
 namespace LoLUpdater
+
+
+
 {
     public partial class MainWindow : Window
     {
+
+        ResourceManager res_man;    // declare Resource manager to access to specific cultureinfo
+        CultureInfo cul;  
+
+
+
+
         private LoLFiles lolFiles = new LoLFiles();
         private void AdobeAIR_Checked(object sender, RoutedEventArgs e)
         {
@@ -558,5 +570,15 @@ namespace LoLUpdater
         {
             CGCheck();
         }
+        void switch_language(void)
+{
+ if (vietnameseToolStripMenuItem.Checked == true)    //in vietnamese
+ { 
+   cul = CultureInfo.CreateSpecificCulture("vi");    //create culture for vietnamese
+ }          
+ else                                                //in english
+ {
+  cul = CultureInfo.CreateSpecificCulture("en");     //create culture for english
+ }
     }
 }
