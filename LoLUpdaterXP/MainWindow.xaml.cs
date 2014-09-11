4b1e383184ff88f5c82fc102bcf9dec62f2ca265
Ilja Korsun <ilja.korsun@gmail.com>
2014-09-11 23:41:26 +0200

    perfect

--- LoLUpdaterXP/MainWindow.xaml.cs
+++ LoLUpdaterXP/MainWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Principal;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
