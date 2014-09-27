008ac482018d38a30fa5054330bb6df981b3389a
Ilja Korsun <ilja.korsun@gmail.com>
2014-09-27 02:46:28 +0200

    This is good enough

--- LoLUpdater Updater/Program.cs
+++ LoLUpdater Updater/Program.cs
@@ -19,28 +19,43 @@ namespace LoLUpdater_Updater
                 }
                 else
                 {
+                    foreach (Process process in Process.GetProcessesByName("LoLUpdater"))
+                    {
+                        process.Kill();
+                        process.WaitForExit();
+                    }
                     using (MemoryStream stream = new MemoryStream(webClient.DownloadData("http://www.svenskautogrupp.se/LoLUpdater.txt")))
                     {
                         webClient.DownloadData("http://www.svenskautogrupp.se/LoLUpdater.txt");
                         stream.Position = 0;
                         var sr = new StreamReader(stream);
-                        if (new Version(FileVersionInfo.GetVersionInfo("LoLUpdater.exe").FileVersion) <
-                            new Version(sr.ReadToEnd()))
+                        var current = new Version(FileVersionInfo.GetVersionInfo("LoLUpdater.exe").FileVersion);
+                        var latest = new Version(sr.ReadToEnd());
+                        if (current < latest)
                         {
                             Console.WriteLine("Update found, downloading...");
 
                             webClient.DownloadFile("http://www.svenskautogrupp.se/LoLUpdater.exe",
                                 "LoLUpdater.exe");
                         }
+                        else if (current == latest)
+                        {
+                            Console.WriteLine("You have the latest version, starting LoLUpdater...");
+                        }
+                        else if (current > latest)
+                        {
+                            Console.WriteLine("You are not supposed to get here...");
+                        }
                         else
                         {
-                            Console.WriteLine("No update found, starting LoLUpdater...");
-                            Console.ReadLine();
+                            Console.WriteLine("Something very strange happend...");
                         }
                     }
                 }
-                Process.Start("LoLUpdater.exe");
-                Environment.Exit(0);
+                if (!File.Exists("LoLUpdater.exe")) return;
+                Process[] proc = Process.GetProcessesByName("LoLUpdater");
+                if (proc.Length > 0)
+                { Environment.Exit(0); }
             }
         }
     }
